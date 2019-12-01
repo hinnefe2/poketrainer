fca11a4e6c246db0e2cbe508a12684cb94ebb52a
hinnefe2 <hinnefe2@illinois.edu>
2019-12-01 15:17:59 -0600

    ENH Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+from os import path
+
+setup(
+    name='poketrainer',
+    version='0.1.0',
+    description='Webapp for gamifying personal training with pokemon',
+    url='https://tbd.herokuapp.com/',
+    packages=find_packages(),
+)
