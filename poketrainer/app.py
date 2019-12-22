d393c45342e967c86ec263345331b23c53337703
hinnefe2 <hinnefe2@illinois.edu>
2019-12-22 17:01:12 -0600

    ENH Force flask to use https for everything

--- poketrainer/app.py
+++ poketrainer/app.py
@@ -2,18 +2,27 @@ import logging
 import os
 
 import connexion
-import requests as req
 
-from flask import render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
-
 
 # Create the Connexion application instance
 connex_app = connexion.App(__name__, specification_dir='./')
 
 # Get the underlying Flask app instance
 flask_app = connex_app.app
+
+# Default to HTTPS (OAuth things require it, and no harm in using it generally)
+# c.f https://stackoverflow.com/a/37842465
+class ForceHTTPS(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+        environ['wsgi.url_scheme'] = 'https'
+        return self.app(environ, start_response)
+
+flask_app.wsgi_app = ForceHTTPS(flask_app.wsgi_app)
 
 # Set up the sqlalchemy database integration
 flask_app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
